6726e5002488c2e990e6747b63324c46cfc5557b
juuperman <jperman@etrr.edu.ar>
2026-09-07 11:21:53 -0300

    Update Ejercicio2_07-09.c

--- Ejercicio2_07-09.c
+++ Ejercicio2_07-09.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
-int vector [4];
-int x,suma;
+int vector[4];
+int x, suma;
+float promedio; 
+
 int main()
 {
-    for(x=0;x<4;x++){
+    for(x=0; x<4; x++){
         printf("Indique el numero a guardar: ");
         scanf("%d", &vector[x]);
-        suma+=vector[x];
+        suma += vector[x];
     }
+    
+    promedio = suma /4; 
     printf("La suma es: %d\n", suma);
+    printf("El promedio es: %.2f\n", promedio); 
     return 0;
 }
